f42aac6eef60bf12b4ef03ae0592e9e151817eae
bin <yan96in@163.com>
2017-06-23 10:31:26 +0800

    treenode

---
+++ src/algorithm/tree/TreeNode1.java
@@ -0,0 +1,23 @@
+package algorithm.tree;
+
+public class TreeNode1 {
+	private int data;
+	private int parent;
+
+	public int getData() {
+		return data;
+	}
+
+	public void setData(int data) {
+		this.data = data;
+	}
+
+	public int getParent() {
+		return parent;
+	}
+
+	public void setParent(int parent) {
+		this.parent = parent;
+	}
+
+}
---
+++ src/algorithm/tree/TreeNode2.java
@@ -0,0 +1,27 @@
+package algorithm.tree;
+
+import algorithm.datastructure.Link;
+
+public class TreeNode2 {
+	private int data;
+	private int parent;
+	private Link children;
+	public int getData() {
+		return data;
+	}
+	public void setData(int data) {
+		this.data = data;
+	}
+	public int getParent() {
+		return parent;
+	}
+	public void setParent(int parent) {
+		this.parent = parent;
+	}
+	public Link getChildren() {
+		return children;
+	}
+	public void setChildren(Link children) {
+		this.children = children;
+	}
+}
